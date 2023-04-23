7a50759998d711bc896078477f7249f5bb06b018
weiseng18 <tws.hci@gmail.com>
2023-04-23 18:41:05 +0800

    add vertical cropping

--- generate_background.m
+++ generate_background.m
@@ -1,5 +1,6 @@
 %{
     Given an image of size R x C, and target height H, target width W.
+    delta specifies an amount to crop from the top and bottom of the new image.
 
     Upscale the image by a factor of H/R.
     Apply blur.
@@ -7,12 +8,12 @@
 
     Note that H is used for the scaling, and W is used for the cropping.
 %}
-function img = generate_background(H, W, image)
+function img = generate_background(H, W, image, delta)
     R = size(image, 1);
     C = size(image, 2);
 
     % Upscale the image by a factor of H/R.
-    ratio = H / R;
+    ratio = (H + delta * 2) / R;
     img = imresize(image, ratio);
 
     % Apply blur.
@@ -20,5 +21,6 @@ function img = generate_background(H, W, image)
 
     % Crop the center of the image to size H x W.
     left = size(img, 2) / 2 - W / 2;
-    img = imcrop(img, [left, 0, W, H]);
+    top = delta;
+    img = imcrop(img, [left, top, W, H]);
 end
--- main.m
+++ main.m
@@ -6,7 +6,8 @@ function [] = main(IMAGE_PATH, HEIGHT, WIDTH)
     fg = imresize(I, 2);
 
     % Generate background
-    bg = generate_background(HEIGHT, WIDTH, I);
+    delta = 60;    % hardcoded for this image, change if needed
+    bg = generate_background(HEIGHT, WIDTH, I, delta);
 
     % Overlay image onto the center of the background
     res = overlay_image(fg, bg);
